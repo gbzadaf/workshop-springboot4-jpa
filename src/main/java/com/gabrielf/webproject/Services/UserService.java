78c3c4a567c800b1797312069db652e5f7a08c93
gbzadaf <fonsecagb23@gmail.com>
2026-03-22 14:04:49 -0300

    Service layer, component registration

---
+++ src/main/java/com/gabrielf/webproject/Services/UserService.java
@@ -0,0 +1,26 @@
+package com.gabrielf.webproject.Services;
+
+import com.gabrielf.webproject.entities.User;
+import com.gabrielf.webproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+       Optional<User> obj = repository.findById(id);
+       return obj.get();
+    }
+}
