ea36e59baa0cb20045ed7c85f8e19a28511d879e
gbzadaf <fonsecagb23@gmail.com>
2026-03-25 15:31:03 -0300

    User delete

--- src/main/java/com/gabrielf/webproject/resources/UserResource.java
+++ src/main/java/com/gabrielf/webproject/resources/UserResource.java
@@ -39,4 +39,10 @@ public class UserResource {
 
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
